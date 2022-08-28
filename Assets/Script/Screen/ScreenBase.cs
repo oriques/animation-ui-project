2eacca4764da350a7b33292689c9df4adc736373
Oriques de Souza <104857614+oriques@users.noreply.github.com>
2022-08-27 23:05:35 -0300

    Add Button Animation, Particles and Main Menu / Shop

--- Assets/Script/Screen/ScreenBase.cs
+++ Assets/Script/Screen/ScreenBase.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using NaughtyAttributes;
 using DG.Tweening;
 
@@ -18,7 +19,9 @@ namespace Screens
         public ScreenType screenType;
 
         public List<Transform> listOfObjects;
+        public List<Typper> listOfPhrases;
 
+        public Image uiBackground;
         public bool startHided = false;
 
         [Header("Animation")]
@@ -27,46 +30,57 @@ namespace Screens
 
         private void Start()
         {
-            if (startHided)
+            if(startHided)
             {
                 HideObjects();
-
             }
         }
 
-
         [Button]
-        protected virtual void Show()
+        public virtual void Show()
         {
-            ShowObjects();
             Debug.Log("Show");
+            ShowObjects();
         }
         [Button]
-        protected virtual void Hide()
+        public virtual void Hide()
         {
-            HideObjects();
             Debug.Log("Show");
+            HideObjects();
         }
 
         private void HideObjects()
         {
             listOfObjects.ForEach(i => i.gameObject.SetActive(false));
+            uiBackground.enabled = false;
         }
+
         private void ShowObjects()
         {
-            for (int i = 0; i < listOfObjects.Count; i++)
-            {
-                var obj = listOfObjects[i];
+          for(int i = 0; i < listOfObjects.Count; i++)
+              {
+                  var obj = listOfObjects[i];
 
-                obj.gameObject.SetActive(true);
-                obj.DOScale(0, animationDuration).From().SetDelay(i * delayBetweenObjects);
+                  obj.gameObject.SetActive(true);
+                  obj.DOScale(0, animationDuration).From().SetDelay(i * delayBetweenObjects);
+              }
+
+            Invoke(nameof(StartType), delayBetweenObjects * listOfObjects.Count);
+            uiBackground.enabled = true;
+        }
+
+        private void StartType()
+        {
+            for (int i = 0; i < listOfPhrases.Count; i++)
+            {
+                listOfPhrases[i].StartType();
             }
         }
+
         private void ForceShowObjects()
         {
             listOfObjects.ForEach(i => i.gameObject.SetActive(true));
         }
-
 
     }
 }
